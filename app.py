08aad61190e45b6785309a4f83c4eb9407b5743f
Davi Gomes <davigomes@alu.ufc.br>
2023-12-04 22:02:49 -0300

    Update project

--- app.py
+++ app.py
@@ -4,18 +4,22 @@ from consumidor_dentro import RabbitmqConsumer_in
 from consumidor_fora import RabbitmqConsumer_out
 
 app = Flask(__name__)
+app.secret_key = "gado"
 
 # Listas para armazenar as mensagens recebidas
 mensagens_dentro = []
 mensagens_fora = []
 
+mensagens_dentro.append("iniciando")
+mensagens_fora.append("iniciando")
+
 def minha_callback_dentro(ch, method, properties, body):
   # Adiciona a mensagem na lista de mensagens
-  mensagens_dentro.append(body)
+  mensagens_dentro.append(body.decode("utf-8"))
 
 def minha_callback_fora(ch, method, properties, body):
   # Adiciona a mensagem na lista de mensagens
-  mensagens_fora.append(body)
+  mensagens_fora.append(body.decode("utf-8"))
 
 # Inicializa os consumidores
 rabbitmq_consumer_in = RabbitmqConsumer_in(minha_callback_dentro)
@@ -25,10 +29,10 @@ rabbitmq_consumer_out = RabbitmqConsumer_out(minha_callback_fora)
 threading.Thread(target=rabbitmq_consumer_in.start).start()
 threading.Thread(target=rabbitmq_consumer_out.start).start()
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def home():
   # Renderiza um template HTML com as mensagens
   return render_template('index.html', mensagens_dentro=mensagens_dentro[-1], mensagens_fora=mensagens_fora[-1])
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(debug=True, port=5004)
--- sensor.py
+++ sensor.py
@@ -26,13 +26,19 @@ while True:
   if action == "in" and gados_fora > 0:
       gados_dentro += 1
       gados_fora -= 1
+
       message = f'Dentro: {gados_dentro}, tempo de execucao: {chamada}'
+      message_fora = f'Fora: {gados_fora}, tempo de execucao: {chamada}'
       produtor_dentro.send_message(message)
+      produtor_fora.send_message(message_fora)
   elif action == "out" and gados_dentro <= TOTAL_GADOS:
       gados_dentro -= 1
       gados_fora += 1
-      message = f'Fora: {gados_fora}, tempo de execucao: {chamada}'
-      produtor_fora.send_message(message)
+
+      message = f'Dentro: {gados_dentro}, tempo de execucao: {chamada}'
+      message_fora = f'Fora: {gados_fora}, tempo de execucao: {chamada}'
+      produtor_dentro.send_message(message)
+      produtor_fora.send_message(message_fora)
   else: 
      message = "Todos os gados ja estao dentro!"
      message_fora = "Nenhum gado esta fora!"
